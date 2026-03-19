fd6244ddb288a5ce9bfcb55d245e64ef3f76bcc2
Nicole Flores <floresiana84@gmail.com>
2026-03-18 23:37:09 -0600

    Crea componente Alert -Implementa tailwind css en vista contacto-index mediante CDN -Crea componente alert y lo implmenta en vista de index

--- resources/views/contactos/contacto-index.blade.php
+++ resources/views/contactos/contacto-index.blade.php
@@ -3,10 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Contactos</title>   
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Document</title>   
 </head>
 <body>
    <h1>Lista de Contactos</h1>
+
+      <x-alert>
+        Mensaje personalizado para la alerta.
+    </x-alert>
     <p>
         <a href="{{ route('contactos.create') }}">Enviar nuevo mensaje de contacto</a>
     </p>
